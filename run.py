afc43ab392978b678c8f170954f3c8991d5c6fae
Kamran Arbaz <kamranarbaz@example.com>
2025-12-30 19:22:36 +0530

    Performance related updates

--- run.py
+++ run.py
@@ -2,5 +2,8 @@ from app import create_app
 
 app = create_app()
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
 # Vercel expects the WSGI application to be named 'app'
 # This allows Vercel to import and run the Flask application
